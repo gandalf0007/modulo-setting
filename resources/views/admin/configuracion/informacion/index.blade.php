e1d9e3924eb079e59fd03abf402c40158e73c5ea
matias <elitegames@hotmail.com.ar>
2018-06-02 19:11:07 -0300

    fix filemanager add youtube and class img-responsive

--- resources/views/admin/configuracion/informacion/index.blade.php
+++ resources/views/admin/configuracion/informacion/index.blade.php
@@ -125,6 +125,73 @@
     filebrowserUploadUrl: 'laravel-filemanager/upload?type=Files&_token=',
     customConfig: 'config.js'
 });
+
+//me hace responsiva la imagen agregando img-responsive en la classe automaticamente
+    CKEDITOR.on('instanceReady', function(ev) {
+        ev.editor.dataProcessor.htmlFilter.addRules(
+                {
+                    elements:
+                            {
+                                $: function(element) {
+                                    // Output dimensions of images as width and height
+                                    if (element.name == 'img') {
+                                        var style = element.attributes.style;
+                                        //responzive images
+
+                                        //declare vars
+                                        var tclass = "";
+                                        var add_class = false;
+
+                                        tclass = element.attributes.class;
+
+                                        //console.log(tclass);
+                                        //console.log(typeof (tclass));
+
+                                        if (tclass === "undefined" || typeof (tclass) === "undefined") {
+                                            add_class = true;
+                                        } else {
+                                            //console.log("I am not undefined");
+                                            if (tclass.indexOf("img-responsive") == -1) {
+                                                add_class = true;
+                                            }
+                                        }
+
+                                        if (add_class) {
+                                            var rclass = (tclass === undefined || typeof (tclass) === "undefined" ? "img-responsive" : tclass + " " + "img-responsive");
+                                            element.attributes.class = rclass;
+                                        }
+
+                                        if (style) {
+                                            // Get the width from the style.
+                                            var match = /(?:^|\s)width\s*:\s*(\d+)px/i.exec(style),
+                                                    width = match && match[1];
+
+                                            // Get the height from the style.
+                                            match = /(?:^|\s)height\s*:\s*(\d+)px/i.exec(style);
+                                            var height = match && match[1];
+
+                                            if (width) {
+                                                element.attributes.style = element.attributes.style.replace(/(?:^|\s)width\s*:\s*(\d+)px;?/i, '');
+                                                element.attributes.width = width;
+                                            }
+
+                                            if (height) {
+                                                element.attributes.style = element.attributes.style.replace(/(?:^|\s)height\s*:\s*(\d+)px;?/i, '');
+                                                element.attributes.height = height;
+                                            }
+                                        }
+                                    }
+
+
+
+                                    if (!element.attributes.style)
+                                        delete element.attributes.style;
+
+                                    return element;
+                                }
+                            }
+                });
+    });
 </script>
 
 
